1c92b2fee733d88614b24cc44aa0a06c24e7e222
Alex Dyurinsky <dyurinsky92@yandex.ru>
2022-11-17 18:37:53 +0300

    second commit

--- app/script.js
+++ app/script.js
@@ -3,15 +3,13 @@ const input = document.querySelector('.weather-input')
 const items = document.querySelector('.weather-items')
 
 btn.addEventListener('click', () => {
-    console.log(input.value)
     fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${input.value},ru&limit=1&appid=6e3cc78d674daa29a71b23a774f36768`)
     .then(function (resp) { return resp.json() })
     .then(function (data) {
-        console.log(data[0].name)
         fetch(`http://api.openweathermap.org/data/2.5/weather?q=${data[0].name},ru&appid=6e3cc78d674daa29a71b23a774f36768`)
             .then(function (resp) { return resp.json() })
             .then(function (data) {
-                createItem(input.value, data.main.temp, data.weather[0]['description'], data.weather[0]['icon'])
+                createItemWithValue(input.value, data.main.temp, data.weather[0]['description'], data.weather[0]['icon'])
             })
             .catch(function () {
                 // catch any errors
@@ -22,26 +20,31 @@ btn.addEventListener('click', () => {
     });
 })
 
-function createItem(title,temp,desc,img) {
-    const item = document.createElement('div')
-    item.classList.add('weather-item')
+function createItemWithValue(titlevalue,temp,desc,img) {
+    const item = createEl('div', 'weather-item')
+    const title = createEl('div', 'item-city')
+    const deg = createEl('div', 'item-deg')
+    const description = createEl('div', 'item-description')
+    const features = createEl('div', 'features')
+    const deleteEl = createEl('div', 'item-delete')
+
+
+    title.textContent = titlevalue
+    deg.innerHTML = Math.round(temp - 273) + '&deg;';
+    description.textContent = desc
+    features.innerHTML = `<img class='item-image' src="https://openweathermap.org/img/wn/${img}@2x.png">`;
     items.append(item)
-    const title2 = document.createElement('div')
-    title2.classList.add('item-city')
-    title2.textContent = title
-    const deg2 = document.createElement('div')
-    deg2.classList.add('item-deg')
-    deg2.innerHTML = Math.round(temp - 273) + '&deg;';
-    const description2 = document.createElement('div')
-    description2.classList.add('item-description')
-    description2.textContent = desc
-    const features2 = document.createElement('ul')
-    features2.classList.add('features')
-    const li2 = document.createElement('li')
-    const delete2 = document.createElement('div')
-    delete2.classList.add('item-delete')
-    delete2.textContent = 'x'
-    features2.append(li2)
-    features2.innerHTML = `<img class='item-image' src="https://openweathermap.org/img/wn/${img}@2x.png">`;
-    item.append(title2, deg2, description2, features2, delete2)
+    item.append(title, deg, description, features, deleteEl)
+    deleteEl.textContent = 'x'
+
+    deleteEl.addEventListener('click', () => {
+        items.remove(item)
+    })
 }
+
+function createEl(tag, className = '') {
+    let createdEl = document.createElement(tag)
+    createdEl.classList.add(className)
+    return createdEl
+}
+
