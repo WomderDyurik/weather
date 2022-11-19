dc4a0ba10c55e4cd1aeaa5f8adad9fa07e1e8261
Alex Dyurinsky <dyurinsky92@yandex.ru>
2022-11-19 14:34:08 +0300

    more useful version

--- app/script.js
+++ app/script.js
@@ -2,48 +2,50 @@ const btn = document.querySelector('.weather-btn')
 const input = document.querySelector('.weather-input')
 const items = document.querySelector('.weather-items')
 const cityItems = document.querySelector('.city-items')
+let check = true
 
 btn.addEventListener('click', () => {
     fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${input.value}&limit=3&appid=6e3cc78d674daa29a71b23a774f36768`)
     .then(function (resp) { return resp.json() })
     .then(function (data) {
+        console.log(data)
         data.forEach(el => {
-            const cityItem = createEl('div', 'city-item')
-            cityItems.append(cityItem)
-            cityItem.textContent = `${el.name}, ${el.country}`
-            cityItem.addEventListener('click', () => {
-                searchWeather(el.name, el.country)
-            })
-            input.addEventListener('focusin', () => {
-                cityItem.remove()
-            })
+            searchWeather(el.name, el.country)
+            // cityItems.append(cityItem)
+            // cityItem.textContent = `${el.name}, ${el.country}`
+            // cityItem.addEventListener('click', () => {
+            //     searchWeather(el.name, el.country)
+            // })
         });
-        
     })
     .catch(function () {
         // catch any errors
     });
+    check = !check
+    
 })
 
 
 
 
-function createItemWithValue(titlevalue,temp,desc,img) {
+function createItemWithValue(titlevalue, country, temp, desc, img) {
     const item = createEl('div', 'weather-item')
     const title = createEl('div', 'item-city')
     const deg = createEl('div', 'item-deg')
     const description = createEl('div', 'item-description')
     const features = createEl('div', 'features')
     const deleteEl = createEl('div', 'item-delete')
+    const itemRight = createEl('div', 'item-right')
 
 
-    title.textContent = titlevalue
+    title.textContent = `${titlevalue} ${country}`
     deg.innerHTML = Math.round(temp - 273) + '&deg;';
     description.textContent = desc
     features.innerHTML = `<img class='item-image' src="https://openweathermap.org/img/wn/${img}@2x.png">`;
-    items.append(item)
-    item.append(title, deg, description, features, deleteEl)
+    itemRight.append(deg, description, features, deleteEl)
+    item.append(title, itemRight)
     deleteEl.textContent = 'x'
+    items.append(item)
 
     deleteEl.addEventListener('click', () => {
         item.remove()
@@ -56,11 +58,11 @@ function createEl(tag, className = '') {
     return createdEl
 }
 
-function searchWeather(name, country){
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${name},${country}&appid=6e3cc78d674daa29a71b23a774f36768`)
+function searchWeather(name, country, state){
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${name},${country}&limit=1&appid=6e3cc78d674daa29a71b23a774f36768`)
             .then(function (resp) { return resp.json() })
-            .then(function (data) {
-                createItemWithValue(input.value, data.main.temp, data.weather[0]['description'], data.weather[0]['icon'])
+            .then(data2 => {
+                createItemWithValue(name, country, data2.main.temp, data2.weather[0]['description'], data2.weather[0]['icon'])
             })
             .catch(function () {
                 // catch any errors
